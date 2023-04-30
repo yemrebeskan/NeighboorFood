2801fb6441db7e5757e535e68607b53907c2927a
gozdekurtulmus <gozde@tap.work>
2023-04-30 22:05:22 +0300

    update Edit About

--- ui/src/components/chefpageComponents/ChefAbout.js
+++ ui/src/components/chefpageComponents/ChefAbout.js
@@ -1,14 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaPen } from 'react-icons/fa'
 
 const ChefAbout = ({ isChef, about }) => {
+  const [isEditing, setIsEditing] = useState(false)
+  const [aboutText, setAboutText] = useState(about)
+
+  const handleSave = () => {
+    // Save changes to the server here
+    setIsEditing(false)
+  }
+
   return (
     <div>
-      {isChef && (
-        <button className="mt-2 bg-green-700 text-white px-4 py-2 rounded-md">
-          Edit About
-        </button>
+      {isEditing ? (
+        <textarea
+        cols={100}
+          style={{ minWidth: 1300 }}
+          value={aboutText}
+          onChange={(e) => setAboutText(e.target.value)}
+          className="mt-2 w-full h-32 rounded-md border-gray-300 focus:border-green-500 focus:ring-2 focus:ring-green-500"
+        ></textarea>
+      ) : (
+        <pre className="font-light whitespace-pre-wrap">{aboutText}</pre>
       )}
-      <pre className="font-light whitespace-pre-wrap">{about}</pre>
+      {isChef && (
+        <div className='flex flex-row-reverse'>
+          <button
+            onClick={() => setIsEditing(!isEditing)}
+            className=" mt-2 mb-2 bg-green-800 text-white px-4 py-2 rounded-md"
+          >
+            {isEditing ? 'Save' : <FaPen className="fas fa-pen"></FaPen>}
+          </button>
+        </div>
+      )}
     </div>
   )
 }
