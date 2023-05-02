0baadced709c0a93e7d7efd8447a711e3efcde99
Gencay Turgut <gencayturgut2@gmail.com>
2023-05-02 20:25:45 +0300

    favorites page update

--- ui/src/components/navbarComponents/ChefCard.js
+++ ui/src/components/navbarComponents/ChefCard.js
@@ -1,18 +1,29 @@
-import React from 'react'
-import './ChefCard.css'
-import star from './star-rating-icon-4.png'
+import React from 'react';
+import './ChefCard.css';
+import { AiOutlineHeart, AiFillHeart } from 'react-icons/ai';
+import StarRating from '../chefpageComponents/StarRating';
+
 const ChefCard = (props) => {
   const handleDelete = () => {
-    props.onDelete(props.id)
-  }
-  return (
-    <div className="chefcard">
-      <div className="name" >{props.name}</div>
-      <div className="flex"><img className="chefPhoto" src={props.image} alt={props.name} /></div>
-      <div className="chefRating"><img src={star}/>{props.rating}</div>
-      <button onClick={handleDelete}> DeleteButton</button>  
-    </div>
-  )
-}
+    props.onDelete(props.id);
+  };
 
-export default ChefCard
+  return (
+    <div className="chefcard block">
+      <div className="block">
+        <button onClick={handleDelete} className="heart text-red-600 cursor-pointer scale-150 flex" size="1.5em">
+          <AiFillHeart />
+        </button>
+        <div className="name flex font-bold">{props.name}</div>
+      </div>
+      <div className="flex">
+        <img className="chefPhoto" src={props.image} alt={props.name} />
+      </div>
+      <div className="chefRating font-bold">
+        <StarRating stars={props.rating} totalVotes={props.totalVotes} />
+      </div>
+    </div>
+  );
+};
+
+export default ChefCard;
