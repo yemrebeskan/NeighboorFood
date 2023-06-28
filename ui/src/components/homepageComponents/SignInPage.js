25d27b5c6694e5dc65617d18f88a6565bfdedbbc
Gencay Turgut <gencayturgut2@gmail.com>
2023-06-28 19:30:44 +0300

    error modal on signin page

--- ui/src/components/homepageComponents/SignInPage.js
+++ ui/src/components/homepageComponents/SignInPage.js
@@ -1,90 +1,96 @@
-import React, { useState, useEffect, useContext } from 'react'
-import './signin.css'
-import AuthContext from '../../context/AuthContext'
-import axios from 'axios'
+import React, { useState, useEffect, useContext } from 'react';
+import './signin.css';
+import AuthContext from '../../context/AuthContext';
+import axios from 'axios';
+import ErrorModal from '../../errorModal/errorModal';
 
 const SignInPage = () => {
-  const [enteredEmail, setEnteredEmail] = useState('')
-  const [enteredPassword, setEnteredPassword] = useState('')
-  const [showPassword, setShowPassword] = useState(false)
-  const [formIsValid, setFormIsValid] = useState(false)
+  const [enteredEmail, setEnteredEmail] = useState('');
+  const [enteredPassword, setEnteredPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+  const [formIsValid, setFormIsValid] = useState(false);
+  const [error, setError] = useState(null); 
 
-  const authCtx = useContext(AuthContext)
+  const authCtx = useContext(AuthContext);
 
   useEffect(() => {
     const identifier = setTimeout(() => {
       setFormIsValid(
         enteredEmail.includes('@') && enteredPassword.trim().length > 6
-      )
-    }, 500)
+      );
+    }, 500);
 
     return () => {
-      clearTimeout(identifier)
-    }
-  }, [enteredEmail, enteredPassword])
+      clearTimeout(identifier);
+    };
+  }, [enteredEmail, enteredPassword]);
+
   const togglePasswordVisibility = (event) => {
-    event.preventDefault()
-    setShowPassword(!showPassword)
-  }
+    event.preventDefault();
+    setShowPassword(!showPassword);
+  };
 
   const emailChangeHandler = (event) => {
-    event.preventDefault()
-    setEnteredEmail(event.target.value)
+    event.preventDefault();
+    setEnteredEmail(event.target.value);
     setFormIsValid(
       event.target.value.includes('@') && enteredPassword.trim().length > 6
-    )
-  }
+    );
+  };
 
   const passwordChangeHandler = (event) => {
-    event.preventDefault()
-    setEnteredPassword(event.target.value)
+    event.preventDefault();
+    setEnteredPassword(event.target.value);
     setFormIsValid(
       enteredEmail.includes('@') && event.target.value.trim().length > 6
-    )
-  }
+    );
+  };
 
   const submitHandler = async (event) => {
-    const signInInfo = { email: enteredEmail, password: enteredPassword }
-    event.preventDefault()
+    const signInInfo = { email: enteredEmail, password: enteredPassword };
+    event.preventDefault();
     try {
       const res = await axios.post(
         'http://127.0.0.1:3001/api/v1/users/login',
         signInInfo
-      )
+      );
       if (res.data.status === 'success') {
-        localStorage.setItem('uid', res.data.uid)
+        localStorage.setItem('uid', res.data.uid);
         localStorage.setItem(
           'userInfo',
           JSON.stringify({ email: signInInfo.email })
-        )
-        const userRole = res.data.role
+        );
+        const userRole = res.data.role;
         //const chefId = res.data.chefId
 
         //authCtx.setUserData(userRole, chefId)
-        authCtx.setUserData(userRole)
+        authCtx.setUserData(userRole);
         authCtx.onLogin({
           email: enteredEmail,
           password: enteredPassword,
-        })
-      } else {
-        console.log('Wrong password or email')
-      }
+        });
+      } 
       // assump log in is successful
     } catch (err) {
-      console.log(err)
+      setError('Wrong password or email'); 
     }
-  }
+  };
 
   const exitHandlerSignInPage = (event) => {
-    event.preventDefault()
-    authCtx.exitHandler()
-  }
+    event.preventDefault();
+    authCtx.exitHandler();
+  };
 
   const createAnAccountHandler = (event) => {
-    event.preventDefault()
-    authCtx.exitHandler()
-    authCtx.handleSignUp()
-  }
+    event.preventDefault();
+    authCtx.exitHandler();
+    authCtx.handleSignUp();
+  };
+
+  const closeModal = () => {
+    setError(null); 
+  };
+
   return (
     <div className="signin rounded">
       <img
@@ -139,8 +145,13 @@ const SignInPage = () => {
           <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/706px-Google_%22G%22_Logo.svg.png" />
         </button>
       </form>
+      <ErrorModal
+        isOpen={error !== null}
+        errorMessage={error}
+        onClose={closeModal}
+      /> 
     </div>
-  )
-}
+  );
+};
 
-export default SignInPage
+export default SignInPage;
