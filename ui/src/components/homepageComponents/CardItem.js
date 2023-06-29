0aadb2a6b8a53ce428da90c9832b92ef0cd4864e
gozdekurtulmus <gozde@tap.work>
2023-06-30 01:20:00 +0300

    For chefs, remove all 'beChef's and Chef Control

--- ui/src/components/homepageComponents/CardItem.js
+++ ui/src/components/homepageComponents/CardItem.js
@@ -1,20 +1,48 @@
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import chefImg from './chef_img.jpeg'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
 const CardItem = () => {
+  const uid = localStorage.getItem('uid')
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await axios.get(
+          `http://127.0.0.1:3001/api/v1/users/${uid}`
+        )
+        setUser(response.data.data.user)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchUser()
+  }, [uid])
+
   return (
     <div className="sm:w-96 w-72 border-solid border-2 border-teal-700 p-4 grid">
       <img src={chefImg} className="mb-4 place-self-center"></img>
-      <p className='sm:text-[16px] text-[10px]'>
+      <p className="sm:text-[16px] text-[10px]">
         The day had begun on a bright note. The sun finally peeked through the
         rain for the first time in a week, and the birds were sinf=ging in its
         warmth. There was no way to anticipate what was about to happen. It was
         a worst-case scenario and there was no way out of it.
       </p>
-      <Link to="/bechef">
-        <button className="sm:text-[16px] text-[12px] border-solid border-2 border-slate-400 mt-4 mb-4 bg-teal-500 mr-4 rounded-lg sm:p-3 p-1 w-28 text-slate-50 hover:bg-teal-700 place-self-center">
-          Be Chef
-        </button>
-      </Link>
+      {user?.isChef ? (
+        <NavLink to={`/chef/${uid}`}>
+          <button className="sm:text-[16px] text-[12px] border-solid border-2 border-slate-400 mt-4 mb-4 bg-teal-500 mr-4 rounded-lg sm:p-3 p-1 w-28 text-slate-50 hover:bg-teal-700 place-self-center">
+            My Chef Page
+          </button>
+        </NavLink>
+      ) : (
+        <Link to="/bechef">
+          <button className="sm:text-[16px] text-[12px] border-solid border-2 border-slate-400 mt-4 mb-4 bg-teal-500 mr-4 rounded-lg sm:p-3 p-1 w-28 text-slate-50 hover:bg-teal-700 place-self-center">
+            Be Chef
+          </button>
+        </Link>
+      )}
     </div>
   )
 }
--- ui/src/components/navbarComponents/DropdownMenu.js
+++ ui/src/components/navbarComponents/DropdownMenu.js
@@ -1,13 +1,13 @@
-import React, { useContext } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import React, { useContext, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 import AuthContext from '../../context/AuthContext'
+import axios from 'axios'
 
-const DropdownMenu = () => {
+const DropdownMenu = ({user}) => {
   const authCtx = useContext(AuthContext)
   const navigation = useNavigate()
 
   return (
-    //   {authCtx.isAdmin  && ()} bu eklencek chefcontrol için
     <ul className="absolute sm:right-7 right-5 md:mt-12 mt-16 bg-white shadow-lg rounded-md text-green-800 md:w-48 w-36 scale-125 z-10 sm:text-sm text-xs">
       <li className="hover:bg-gray-100 rounded-t-md">
         <div
@@ -42,15 +42,17 @@ const DropdownMenu = () => {
           Settings
         </div>
       </li>
-      <li className="hover:bg-gray-100">
-        <div
-          onClick={() => navigation('/bechef')}
-          className="w-full text-left p-4"
-        >
-          Be Chef
-        </div>
-      </li>
-
+      {!user?.isChef && (
+        <li className="hover:bg-gray-100">
+          <div
+            onClick={() => navigation('/bechef')}
+            className="w-full text-left p-4"
+          >
+            Be Chef
+          </div>
+        </li>
+      )}
+      {user?.isAdmin && (
       <li className="hover:bg-gray-100">
         <div
           onClick={() => navigation('/admin/chefcontrol')}
@@ -58,7 +60,7 @@ const DropdownMenu = () => {
         >
           Chef Control
         </div>
-      </li>
+      </li>)}
 
       <li className="hover:bg-gray-100 rounded-b-md">
         <div
