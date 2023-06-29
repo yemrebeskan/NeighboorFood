fe87927c730a4bc95a6c7875a60926b1b08107cc
Gencay Turgut <gencayturgut2@gmail.com>
2023-06-29 18:55:44 +0300

    searchbar city suggestions

--- ui/src/components/homepageComponents/SearchBar.js
+++ ui/src/components/homepageComponents/SearchBar.js
@@ -1,21 +1,116 @@
 import React, { useState } from 'react'
 import './SearchBar.css'
+const cities = [
+  'adana',
+  'adiyaman',
+  'afyonkarahisar',
+  'agri',
+  'aksaray',
+  'amasya',
+  'ankara',
+  'antalya',
+  'ardahan',
+  'artvin',
+  'aydin',
+  'balikesir',
+  'bartin',
+  'batman',
+  'bayburt',
+  'bilecik',
+  'bingol',
+  'bitlis',
+  'bolu',
+  'burdur',
+  'bursa',
+  'canakkale',
+  'cankiri',
+  'corum',
+  'denizli',
+  'diyarbakir',
+  'duzce',
+  'edirne',
+  'elazig',
+  'erzincan',
+  'erzurum',
+  'eskisehir',
+  'gaziantep',
+  'giresun',
+  'gumushane',
+  'hakkari',
+  'hatay',
+  'igdir',
+  'isparta',
+  'istanbul',
+  'izmir',
+  'kahramanmaras',
+  'karabuk',
+  'karaman',
+  'kars',
+  'kastamonu',
+  'kayseri',
+  'kilis',
+  'kirikkale',
+  'kirklareli',
+  'kirsehir',
+  'kocaeli',
+  'konya',
+  'kutahya',
+  'malatya',
+  'manisa',
+  'mardin',
+  'mersin',
+  'mugla',
+  'mus',
+  'nevsehir',
+  'nigde',
+  'ordu',
+  'osmaniye',
+  'rize',
+  'sakarya',
+  'samsun',
+  'sanliurfa',
+  'siirt',
+  'sinop',
+  'sivas',
+  'tekirdag',
+  'tokat',
+  'trabzon',
+  'tunceli',
+  'usak',
+  'van',
+  'yalova',
+  'yozgat',
+  'zonguldak'
+];
 
 const SearchBar = (props) => {
-  const [location, setLocation] = useState('')
+  const [location, setLocation] = useState('');
+  const [suggestions, setSuggestions] = useState([]);
 
   const handleSubmit = (event) => {
-    event.preventDefault()
-    props.onSearch(location.toLowerCase())
-  }
+    event.preventDefault();
+    props.onSearch(location.toLowerCase());
+  };
 
   const handleLocationChange = (event) => {
-    setLocation(event.target.value)
-  }
+    const value = event.target.value.toLowerCase();
+    setLocation(value);
+  
+    const filteredSuggestions = value ? cities.filter((city) => city.startsWith(value)) : [];
+    setSuggestions(filteredSuggestions);
+  };
+  
+
+  const handleSuggestionClick = (suggestion) => {
+    setLocation(suggestion);
+    setSuggestions([]);
+    props.onSearch(suggestion.toLowerCase());
+  };
+  
 
   return (
     <form
-      className="border-solid border-2 border-slate-400 rounded-lg flex"
+    className="border-solid border-2 border-slate-400 rounded-lg flex form"
       onSubmit={handleSubmit}
     >
       <input
@@ -30,8 +125,21 @@ const SearchBar = (props) => {
       >
         <p className="ml-2 mr-2"> Search </p>
       </button>
+      {suggestions.length > 0 && (
+        <ul className="suggestions">
+          {suggestions.map((suggestion) => (
+            <li
+              key={suggestion}
+              className="suggestion"
+              onClick={() => handleSuggestionClick(suggestion)}
+            >
+              {suggestion}
+            </li>
+          ))}
+        </ul>
+      )}
     </form>
-  )
-}
+  );
+};
 
-export default SearchBar
+export default SearchBar;
