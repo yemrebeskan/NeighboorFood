5e0859209a49e35da66bfa8243b17d5f3a939a1b
Gencay Turgut <gencayturgut2@gmail.com>
2023-07-02 15:43:37 +0300

    notification backend connection

--- ui/src/components/navbarComponents/Notifications.js
+++ ui/src/components/navbarComponents/Notifications.js
@@ -1,23 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { BellIcon, CheckCircleIcon, ExclamationIcon } from '@heroicons/react/solid';
-
-// TODO: backende bağlanması lazım
-const notifications = [
-  { id: 1, text: 'Notification 1', isNew: true },
-  { id: 2, text: 'Notification 2', isNew: false },
-  { id: 3, text: 'Notification 3', isNew: false },
-  { id: 4, text: 'Notification 4', isNew: false },
-];
 
 const Notifications = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [hasNewNotification, setHasNewNotification] = useState(true);
+  const [notifications, setNotifications] = useState([]);
+  const uid = localStorage.getItem('uid')
+
+  useEffect(() => {
+    const fetchNotification = async () => {
+      try {
+        const response = await axios.get(
+          `http://127.0.0.1:3001/api/v1/users/${uid}/notifications`
+        );
+        setNotifications(response.data.notifications);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchNotification();
+  }, []);
 
   const handleOpen = () => {
     setIsOpen(!isOpen);
     setHasNewNotification(false);
   };
 
+  const deleteNotification = async (id) => {
+    console.log(id)
+    try {
+      await axios.delete(`http://127.0.0.1:3001/api/v1/users/${id}/notification`);
+      const response = await axios.get(
+        `http://127.0.0.1:3001/api/v1/users/${uid}/notifications`
+      );
+      setNotifications(response.data.notifications);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
   return (
     <div className="relative inline-block text-left">
       <button
@@ -37,7 +59,8 @@ const Notifications = () => {
           <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
             {notifications.map((notification) => (
               <a
-                key={notification.id}
+                key={notification._id}
+                onClick={()=>deleteNotification(notification._id)}
                 href="#"
                 className={`flex items-center px-4 py-3 text-sm leading-5 text-gray-700 hover:bg-gray-100 hover:text-gray-900 ${notification.isNew ? 'bg-yellow-50' : ''}`}
                 role="menuitem"
@@ -47,7 +70,7 @@ const Notifications = () => {
                 ) : (
                   <CheckCircleIcon className="mr-3 h-5 w-5 text-green-400" aria-hidden="true" />
                 )}
-                <span>{notification.text}</span>
+                <span>{notification.message}</span>
               </a>
             ))}
           </div>
