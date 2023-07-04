007341940d0b56624545a3c3e7de1461748784a1
VeeTora <hss.2277@hotmail.com>
2023-07-04 14:31:55 +0300

    profile change(backendi hariç) bitti

--- ui/src/pages/Profile.js
+++ ui/src/pages/Profile.js
@@ -24,6 +24,8 @@ const Profile = () => {
 
         setImage(response.data.data.user.image)
         setUser(response.data.data.user)
+        setAddress(response.data.data.user.district)
+        console.log(response.data.data.user)
       } catch (error) {
         setError('Error fetching user data. Please try again later.')
         console.log(error)
@@ -38,33 +40,73 @@ const Profile = () => {
     }
   }, [authCtx.isLoggedIn, navigate])
 
-  const handleProfileAddressChange = () => {
-    // Profil adresi değiştirme işlemleri
+  const [address, setAddress] = useState(user.district)
+  const handleProfileAddressChange = async () => {
+    try {
+      const addressPush = {
+        district: address,
+      }
+      console.log('Yeni adres gönderme öncesi:', address)
+      const response = await axios.put(
+        `http://127.0.0.1:3001/api/v1/settings/${uid}/address`,
+        addressPush
+      )
+      //window.location.reload()
+      console.log('Yeni adres gönderildi:', address)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const handleAddressInputChange = (event) => {
+    setAddress(event.target.value)
   }
 
   const handleProfileImageChange = async (event) => {
     const file = event.target.files[0]
     const allowedExtensions = /(\.jpg|\.jpeg|\.png)$/i
-
+    const reader = new FileReader()
     if (file && allowedExtensions.exec(file.name)) {
-      const formData = new FormData()
-      formData.append('image', file)
-      console.log(formData)
-      try {
-        const response = await axios.put(
-          `http://127.0.0.1:3001/api/v1/settings/${uid}/image`,
-          formData
-        )
-        // İstek başarılı olduğunda yapılacak işlemler
-        console.log('Resim yüklendi!')
-        navigate('/profile')
-      } catch (error) {
-        setError('Resim yüklenirken hata oluştu!')
-        console.error('Resim yüklenirken hata oluştu:', error)
+      reader.onloadend = async () => {
+        try {
+          const base64Data = reader.result.split(',')[1]
+          const imagedata = {
+            image: base64Data,
+          }
+          const response = await axios.put(
+            `http://127.0.0.1:3001/api/v1/settings/${uid}/image`,
+            imagedata
+          )
+          window.location.reload()
+        } catch (error) {
+          console.log(error)
+        }
       }
+      reader.readAsDataURL(file)
     } else {
       setError('Desteklenmeyen dosya formatı!')
       console.log('Desteklenmeyen dosya formatı!')
+    }
+  }
+  const [phoneNumber, setPhoneNumber] = useState(user.phoneNumber)
+  const handlePhoneInputChange = (event) => {
+    setPhoneNumber(event.target.value)
+  }
+
+  const handleProfilePhoneChange = async () => {
+    try {
+      const phonePush = {
+        phoneNumber: phoneNumber,
+      }
+      console.log('Yeni telefon numarası gönderme öncesi:', phoneNumber)
+      const response = await axios.put(
+        `http://127.0.0.1:3001/api/v1/settings/${uid}/phoneNumber`,
+        phonePush
+      )
+      //window.location.reload()
+      console.log('Yeni telefon numarası gönderildi:', phoneNumber)
+    } catch (error) {
+      console.log(error)
     }
   }
 
@@ -80,13 +122,17 @@ const Profile = () => {
     navigate('/bechef')
   }
 
+  const handleMyChefPage = () => {
+    navigate(`/chef/${uid}`)
+  }
+
   return (
     <div className="flex flex-col items-center justify-center mb-64 max-w mx-auto mt-10 mb-10 p-6 bg-white rounded-lg shadow">
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 items-center justify-center">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 items-center justify-center">
         <div className="m-8  ml-24 right-0 ">
           <div className="mb-4 relative">
             <img
-              src={image}
+              src={`data:image/png;base64,${image}`}
               alt="Profile"
               className="rounded-full w-32 h-32 md:w-32 md:h-32"
             />
@@ -108,30 +154,36 @@ const Profile = () => {
           <h2 className="text-lg md:text-xl font-bold mb-2">
             {user.name} {user.surname}
           </h2>
-
-          <button
-            className="mb-2 md:mb-0 md:mr-2 bg-teal-500 text-white px-4 py-2 rounded"
-            onClick={handleBecomeChef}
-          >
-            Chef Ol
-          </button>
-          <button
-            className="mb-2 md:mb-0 md:mr-2 bg-teal-500 text-white px-4 py-2 rounded"
-            onClick={handleGoToSettings}
-          >
-            Ayarlar
-          </button>
+          {user.isChef ? (
+            <button
+              className="mb-2 md:mb-0 md:mr-2 bg-teal-500 text-white px-4 py-2 rounded"
+              onClick={handleMyChefPage}
+            >
+              My Chef Page
+            </button>
+          ) : (
+            <button
+              className="mb-2 md:mb-0 md:mr-2 bg-teal-500 text-white px-4 py-2 rounded"
+              onClick={handleBecomeChef}
+            >
+              Chef Ol
+            </button>
+          )}
         </div>
         <div className="m-8 flex flex-col items-center">
           <div className="flex items-center mb-4">
             <span className="mr-2">Adres:</span>
             <textarea
-              defaultValue="Kullanıcının Adresi"
+              defaultValue={address}
+              onChange={handleAddressInputChange}
               className="border border-gray-300 rounded px-2 py-1 text-sm md:text-base resize-none flex-grow"
-              rows={4}
+              rows={2}
             />
           </div>
-          <button className="px-3 py-1 mb-5 bg-teal-500 text-white rounded text-sm md:text-base">
+          <button
+            className="px-3 py-1 mb-5 bg-teal-500 text-white rounded text-sm md:text-base"
+            onClick={handleProfileAddressChange}
+          >
             Change Address
           </button>
           <div className="flex justify-center md:justify-between">
@@ -143,14 +195,39 @@ const Profile = () => {
             </button>
           </div>
         </div>
+        <div className="m-8 mt-16 flex flex-col items-center">
+          <div className="flex items-center mb-2">
+            <span className="mr-2">Phone Number:</span>
+            <textarea
+              defaultValue={user.phoneNumber}
+              onChange={handlePhoneInputChange}
+              className="border border-gray-300 rounded px-2 py-1  h-8 w-32  text-sm md:text-base resize-none flex-grow"
+              rows={1}
+            />
+          </div>
+          <button
+            className="px-3 py-1 mb-5 mt-4 bg-teal-500 text-white rounded text-sm md:text-base"
+            onClick={handleProfilePhoneChange}
+          >
+            Change Phone Number
+          </button>
+          <div className="flex justify-center md:justify-between">
+            <button
+              className="mb-2 md:mb-0 md:mr-2 bg-teal-500 text-white px-4 py-2 rounded"
+              onClick={handleGoToSettings}
+            >
+              Ayarlar
+            </button>
+          </div>
+        </div>
+        {error && (
+          <ErrorModal
+            isOpen={error !== null}
+            errorMessage={error}
+            onClose={() => setError(null)}
+          />
+        )}
       </div>
-      {error && (
-        <ErrorModal
-          isOpen={error !== null}
-          errorMessage={error}
-          onClose={() => setError(null)}
-        />
-      )}
     </div>
   )
 }
